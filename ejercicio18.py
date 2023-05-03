c7cab34ea3d7a1e26c1e8e9ef2680407ad989c36
Criss2k23 <129349000+Criss2k23@users.noreply.github.com>
2023-05-03 00:52:41 -0500

    Add files via upload

---
+++ ejercicio18.py
@@ -0,0 +1,8 @@
+import folium
+# Define el mapa mundial centrado alrededor de Lima, Perú con un zoom bajo
+# https://python-graph-gallery.com/288-map-background-with-folium/
+
+world_map = folium.Map(location=[-12.0431800, -77.0282400], zoom_start=12, tiles='Stamen Terrain')
+
+# mostrando el mapa
+world_map
